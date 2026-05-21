b09b6a888bff19e4d7ff77c7399961752eb2880a
cooltoufiq <cooltoufiq@gmail.com>
2026-05-22 02:19:26 +0530

    added row count val

--- row_count_test.py
+++ row_count_test.py
@@ -7,5 +7,5 @@ target_count = 91
 print(f"Source count: {source_count}")
 print(f"Target count: {target_count}")
 if source_count != target_count:
-    print("Row count mismatch!")
+    raise Exception("Row count mismatch!")
 else:    print("Row count matches.")
